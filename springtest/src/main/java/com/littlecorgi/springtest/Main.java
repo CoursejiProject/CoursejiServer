b22305e991d6f1a44965ad36cc619a170d325c84
littlecorgi_twk <a1203991686@126.com>
2021-04-10 13:25:41 +0800

    初始化项目，并添加CheckStyle配置文件

---
+++ springtest/src/main/java/com/littlecorgi/springtest/Main.java
@@ -0,0 +1,8 @@
+package com.littlecorgi.springtest;
+
+/**
+ * @author littlecorgi
+ * @date 2021/4/10
+ */
+public class Main {
+}
---
+++ springtest/src/main/java/com/littlecorgi/springtest/service/UserService.java
@@ -0,0 +1,8 @@
+package com.littlecorgi.springtest.service;
+
+/**
+ * @author littlecorgi
+ * @date 2021/4/10
+ */
+public class UserService {
+}
