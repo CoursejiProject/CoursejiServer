0ef66d1c9375135c6af0628ecc867eb63b46a5bb
littlecorgi_twk <a1203991686@126.com>
2021-04-10 18:52:20 +0800

    通过xml配置ioc

--- springtest/src/main/java/com/littlecorgi/springtest/service/User.java
+++ springtest/src/main/java/com/littlecorgi/springtest/service/User.java
@@ -5,4 +5,55 @@ package com.littlecorgi.springtest.service;
  * @date 2021/4/10
  */
 public class User {
+    long id;
+    String email;
+    String password;
+    String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 构造方法
+     *
+     * @param id       用户ID
+     * @param email    邮箱
+     * @param password 密码
+     * @param name     用户名称
+     */
+    public User(long id, String email, String password, String name) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
 }
