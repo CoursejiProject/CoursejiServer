93abdd9e76c5d754d1991388f7cdafc0b0d3325b
littlecorgi_twk <a1203991686@126.com>
2021-04-25 00:46:18 +0800

    调整响应码结构

---
+++ courseji/src/main/kotlin/com/littlecorgi/courseji/common/constants/ResponseCodeConstants.kt
@@ -0,0 +1,16 @@
+package com.littlecorgi.courseji.common.constants
+
+/**
+ * 响应码相关常量
+ *
+ * @author littlecorgi
+ * @date 2021/4/25
+ */
+object ResponseCodeConstants {
+    const val SUCCESS_CODE = 800
+    const val FAILURE_CODE = 900
+    const val USER_ERROR_CODE = 1000
+    const val COURSE_ERROR_CODE = 1100
+    const val ATTENDANCE_ERROR_CODE = 1200
+    const val LEAVE_ERROR_CODE = 1300
+}
